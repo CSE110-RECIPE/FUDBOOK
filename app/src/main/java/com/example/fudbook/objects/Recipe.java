7ecdec813177e105254ec189b8d78c7524f7e29b
seandao <seandao2011@gmail.com>
2020-05-17 04:41:42 -0700

    Added Book, Recipe, and User classes

--- app/src/main/java/com/example/fudbook/objects/Recipe.java
+++ app/src/main/java/com/example/fudbook/objects/Recipe.java
@@ -1,15 +1,71 @@
 package com.example.fudbook.objects;
 
+/**Stores all recipe information relevant to recipe creation such as title, author, ingredients, instructions, and image.*/
 public class Recipe {
+    private String title;
+    private String author;
+    private String[] ingredients;
+    private String[] instructions;
+    private String[] tags;
+    private String image;
 
-    // instructions
+    //constructor
+    public Recipe(String title, String author, String[] ingr, String[] instr, String[] tags, String image) {
+        this.title = title;
+        this.author = author;
+        ingredients = ingr;
+        instructions = instr;
+        this.tags = tags;
+        this.image = image;
+    }
 
-    // ingredients []
+    //setter methods
+    public void setTitle(String title) {
+        this.title = title;
+    }
 
-    // picture
+    public void setAuthor(String author) {
+        this.author = author;
+    }
 
-    // title
+    public void setIngr(String[] ingr) {
+        this.ingredients = ingr;
+    }
 
-    // setter getter
+    public void setInstr(String[] instr) {
+        this.instructions = instr;
+    }
 
+    public void setImage(String[] tags) {
+        this.tags = tags;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    //getter methods
+    public String getTitle() {
+        return title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public String[] getIngr() {
+        return ingredients;
+    }
+
+    public String[] getInstr() {
+        return instructions;
+    }
+
+    public String[] getTags() {
+        return tags;
+    }
+
+    public String getImage() {
+        return image;
+    }
 }
--- app/src/main/java/com/example/fudbook/objects/User.java
+++ app/src/main/java/com/example/fudbook/objects/User.java
@@ -1,13 +1,71 @@
 package com.example.fudbook.objects;
 
+/**Stores information about the user such as their username, personal bookshelf, and default food preferences.*/
 public class User {
+    private String name;
+    private String[] bookshelf;
+    private String favId;
+    private String myBookId;
+    private String[] include;
+    private String[] exclude;
 
-    // displayName
-    // bookshelf : book ids
-    // favorite book
-    // my book --> stores recipes made by user
-    // filter preferences
+    //constructor
+    public User(String name, String[] bookshelf, String favId, String myBookId, String[] include, String[] exclude) {
+        this.name = name;
+        this.bookshelf = bookshelf;
+        this.favId = favId;
+        this.myBookId = myBookId;
+        this.include = include;
+        this.exclude = exclude;
+    }
 
-    // return username
-    // filter preferences
+    //setter methods
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setBookshelf(String[] bookshelf) {
+        this.bookshelf = bookshelf;
+    }
+
+    public void setFavorite(String favId) {
+        this.favId = favId;
+    }
+
+    public void setMyBook(String myBookId) {
+        this.myBookId = myBookId;
+    }
+
+    public void setIncludePref(String[] include) {
+        this.include = include;
+    }
+
+    public void setExcludePref(String[] exclude) {
+        this.exclude = exclude;
+    }
+
+    //getter methods
+    public String getName() {
+        return name;
+    }
+
+    public String[] getBookshelf() {
+        return bookshelf;
+    }
+
+    public String getFavorite() {
+        return favId;
+    }
+
+    public String getMyBook() {
+        return myBookId;
+    }
+
+    public String[] getIncludePref() {
+        return include;
+    }
+
+    public String[] getExcludePref() {
+        return exclude;
+    }
 }
